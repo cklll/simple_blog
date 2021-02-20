9faa2ebd088bed9fd2cf480e15f89ae46c920657
Chan Kin Long <24862555+cklll@users.noreply.github.com>
2021-02-20 21:04:15 +0800

    Post MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :posts, only: %i[index create new show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
